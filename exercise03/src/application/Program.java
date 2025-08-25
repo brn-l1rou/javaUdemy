1388dfbd4f53295c2e4d6e6724429124b027f150
brn-l1rou <l1roulco61@hotmail.com>
2025-08-24 22:54:41 -0300

    including exercise03

---
+++ exercise03/src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Student;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Student student = new Student();
+		
+		student.name = sc.nextLine();
+		student.grade1 = sc.nextDouble();
+		student.grade2 = sc.nextDouble();
+		student.grade3 = sc.nextDouble();
+		
+		System.out.println("FINAL GRADE = " + String.format("%.2f", student.finalGrade()));
+		if(student.finalGrade() < 60) {
+			System.out.println("FAILED");
+			System.out.println("MISSING " + String.format("%.2f", student.missingPoints()) + " POINTS");
+		}else {
+			System.out.println("PASS");
+		}
+			
+		sc.close();
+
+	}
+
+}
